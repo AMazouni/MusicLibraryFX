f896919fc94bc413798f8b948704da1d62927f33
Ayoub Mazouni <ayoubmazouni@gmail.com>
2020-03-11 19:06:33 +0100

    init

---
+++ src/main/java/org/openjfx/MusicLibraryFX/App.java
@@ -0,0 +1,32 @@
+package org.openjfx.MusicLibraryFX;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import retrofit2.Retrofit;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+        Retrofit retrofit;
+
+        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
