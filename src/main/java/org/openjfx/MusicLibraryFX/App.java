0d4607be51adb354fabd12be7621d472573605da
Mmb <medmehdibelghali@gmail.com>
2020-03-27 17:22:05 +0100

    Changed the FXML file

--- src/main/java/org/openjfx/MusicLibraryFX/App.java
+++ src/main/java/org/openjfx/MusicLibraryFX/App.java
@@ -29,7 +29,7 @@ public class App extends Application {
 	static Button backward = new Button();*/
 	@Override
 	public void start(Stage stage) throws IOException {
-	       Parent root = FXMLLoader.load(getClass().getResource("views/newborderPane.fxml"));
+	       Parent root = FXMLLoader.load(getClass().getResource("views/test.fxml"));
 	       
 	        Scene scene = new Scene(root);
 	        stage.setResizable(false);
